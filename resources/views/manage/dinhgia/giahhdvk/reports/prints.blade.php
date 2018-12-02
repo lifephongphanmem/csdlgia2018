024623c2dc7efe8e512de169a8edffe6e55cd214
minhtranlife <minhtranlife@gmail.com>
2018-12-02 11:06:22 +0700

    Update

--- resources/views/manage/dinhgia/giahhdvk/reports/prints.blade.php
+++ resources/views/manage/dinhgia/giahhdvk/reports/prints.blade.php
@@ -96,7 +96,7 @@
             <td style="text-align: right;font-weight: bold">{{number_format($tt->gia)}}</td>
             <td style="text-align: right;font-weight: bold">{{number_format($tt->gia - $tt->gialk)}}</td>
             <td style="text-align: right;font-weight: bold">{{number_format($tt->gialk) == 0 ? number_format($tt->gia) == 0 ? 0 : 100
-                            : round(number_format($tt->gia)/number_format($tt->giath),2)}}</td>
+                            : round(number_format($tt->gia)/number_format($tt->gialk),2)}}</td>
             <td></td>
         </tr>
     @endforeach
