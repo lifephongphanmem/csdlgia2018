9e413e3a7625efa3a1ea48a1c8acbc6c397399f6
minhtranlife <minhtranlife@gmail.com>
2019-04-12 09:32:03 +0700

    Update

--- app/Http/Controllers/GiaHhDvKController.php
+++ app/Http/Controllers/GiaHhDvKController.php
@@ -325,7 +325,7 @@ class GiaHhDvKController extends Controller
                                 $sheet->loadView('manage.dinhgia.giahhdvk.excel.danhmuc')
                                     ->with('model_nhom', $model_nhom->sortBy('manhom'))
                                     ->with('model', $model)
-                                    ->with('pageTitle', 'Danh mục hàng hóa');
+                                    ->with('pageTitle', 'Danh mục hàng hóa '.$model_nhom->manhom);
                                 //$sheet->setPageMargin(0.25);
                                 $sheet->setAutoSize(false);
                                 $sheet->setFontFamily('Tahoma');
