df5ca0a0ed1d3d15ca6fcf88add86a38edc84334
minhtranlife <minhtranlife@gmail.com>
2019-05-03 14:44:27 +0700

    Update

--- app/Http/Controllers/GiaCacLoaiDatController.php
+++ app/Http/Controllers/GiaCacLoaiDatController.php
@@ -189,7 +189,8 @@ class GiaCacLoaiDatController extends Controller
         $model = GiaCacLoaiDat::findOrFail($id);
         $inputs['capdo'] = $model->capdo+1;
 
-        $inputs['macapdo'] = GiaCacLoaiDat::where('mahuyen',$inputs['mahuyen'])->where('capdo',($model->capdo +1))->where('magoc',$model->maso)->get()->max('macapdo') + 1;
+        $inputs['macapdo'] = GiaCacLoaiDat::where('mahuyen',$inputs['mahuyen'])->where('capdo',($model->capdo +1))->where('magoc',$model->maso)->get()
+                ->max('macapdo') + 1;
         if($model->capdo == 1)
             $inputs['hienthi'] = $model->vitri;
         else
