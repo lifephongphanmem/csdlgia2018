dd71f03cc2c14006351dc29551f954c2f19df63c
minhtranlife <minhtranlife@gmail.com>
2019-09-30 14:35:28 +0700

    Update

--- resources/views/manage/dinhgia/giadvgddt/reports/BcGiaDvGdDt.blade.php
+++ resources/views/manage/dinhgia/giadvgddt/reports/BcGiaDvGdDt.blade.php
@@ -52,7 +52,7 @@
         </td>
     </tr>
 </table>
-<p style="text-align: center;font-weight: bold;font-size: 20px; text-transform: uppercase;">GIÁ DỊCH VỤ GIÁO DỤC VÀ ĐÀO TẠO @if($inputs['district'] != 'All'){{$districts->diaban}}@endif</p>
+<p style="text-align: center;font-weight: bold;font-size: 20px; text-transform: uppercase;">GIÁ DỊCH VỤ GIÁO DỤC VÀ ĐÀO TẠO @if($inputs['district'] != 'All'){{$diabans->diaban}}@endif</p>
 @if($inputs['nam'] != 'all')
 <p style="text-align: center;font-weight: bold;font-size: 16px">Năm {{$inputs['nam']}}</p>
 @endif
