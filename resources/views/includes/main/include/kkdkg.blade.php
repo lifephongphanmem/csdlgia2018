5a826cef406bcd91e0d47c87adf478a05095eede
minhtranlife <minhtranlife@gmail.com>
2019-10-22 09:18:47 +0700

    Update main

--- resources/views/includes/main/include/kkdkg.blade.php
+++ resources/views/includes/main/include/kkdkg.blade.php
@@ -2,7 +2,7 @@
     <li class="tooltips" data-container="body" data-placement="right" data-html="true"
         data-original-title="Tổ chức, cá nhận đăng ký giá theo yêu cầu của Sở Tài chính, sở quản lý ngành">
         <a href="">
-            <span class="title">Mặt hàng bình ổn giá</span>
+            <span class="title">Đăng ký giá mặt hàng trong danh mục bình ổn giá</span>
             <span class="arrow"></span>
         </a>
         <ul class="sub-menu" style="display: none;">
@@ -19,9 +19,9 @@
                         </a>
                         <ul class="sub-menu" style="display: none;">
                             @if(session('admin')->level == 'DN')
-                                <li><a href="{{url('hosokkdkg?manghe='.$dm->manghe)}}">Kê khai giá</a></li>
+                                <li><a href="{{url('hosokkdkg?manghe='.$dm->manghe)}}">Đăng ký giá</a></li>
                             @else
-                                <li><a href="{{url('thongtindnkkgdk?manghe='.$dm->manghe)}}">Kê khai giá</a></li>
+                                <li><a href="{{url('thongtindnkkgdk?manghe='.$dm->manghe)}}">Đăng ký giá</a></li>
                                 <li><a href="{{url('xetduyetkkdkg?manghe='.$dm->manghe)}}">Thông tin hồ sơ xét duyệt</a></li>
                                 <li><a href="{{url('timkiemkkdkg?manghe='.$dm->manghe)}}">Tìm kiếm thông tin đăng ký giá</a></li>
                                 <li><a href="{{url('baocaokekhaidkg?manghe='.$dm->manghe)}}">Báo cáo tổng hợp</a></li>
