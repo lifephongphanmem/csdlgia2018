d7c60053a081acbf6670833d2794e95b7fb8fd13
minhtranlife <minhtranlife@gmail.com>
2019-11-26 15:21:30 +0700

    Update

--- app/Http/Controllers/PhiLePhiController.php
+++ app/Http/Controllers/PhiLePhiController.php
@@ -197,7 +197,7 @@ class PhiLePhiController extends Controller
             $inputs['manhom'] = isset($inputs['manhom']) ? $inputs['manhom'] : '';
             $inputs['ptcp'] = isset($inputs['ptcp']) ? $inputs['ptcp'] : '';
             $model = PhiLePhiCt::Leftjoin('philephi','philephi.mahs','=','philephict.mahs')
-                ->Leftjoin('dmphilephi','dmphilephi.manhom','=','dmphilephi.manhom')
+//                ->Leftjoin('dmphilephi','dmphilephi.manhom','=','dmphilephi.manhom')
                 ->whereIn('philephi.trangthai',['HT','CB'])
                 ->select('philephict.*','philephi.soqd','philephi.ngayapdung','philephi.trangthai','dmphilephi.tennhom');
             if($inputs['nam'] != 'all')
